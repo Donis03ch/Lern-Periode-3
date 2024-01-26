84185687f28d6add3fcc1bb969e164270aab8c91
Donis03ch <111046453+Donis03ch@users.noreply.github.com>
2024-01-26 16:39:34 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+using Aspose.Pdf.Optimization;
+using Aspose.Pdf.Text;
+
+namespace DEBreader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UploadButton_Click(object sender, RoutedEventArgs e)
+        {
+            string sFilename = UploadTxt.Text;
+
+            PdfReader pdf_Reader = new PdfReader(sFilename);
+            string sText = "";
+
+            for (int i = 1; i <= pdf_Reader.NumberOfPages; i++)
+            {
+                sText = sText + PdfTextExtractor.GetTextFromPage(pdf_Reader, i);
+            }
+
+            lblPDF_Output.Text = sText;
+        }
+
+
+    }
+}
